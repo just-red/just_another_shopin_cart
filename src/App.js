50336c35d7704ffc2cf435b3b795237f7ba59f64
Raktim Pradip Barua <raktimbarua88@gmail.com>
2020-09-22 10:03:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import  React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
 
-function App() {
+function App(props) {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid-container">
+      <header>
+        <a  href='/'> React Shopping Cart
+        </a> 
+      </header> 
+
+    <main>Product List</main>
+    <footer>
+      All right is reserved 
+    </footer>
+
+     
     </div>
+
   );
 }
 
