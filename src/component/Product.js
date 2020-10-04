7cf3ad3fb7320597ec8a55403bb59af9b6cfe7b4
Raktim Pradip Barua <raktimbarua88@gmail.com>
2020-10-04 11:53:05 +0530

    Animation-modals

--- src/component/Product.js
+++ src/component/Product.js
@@ -1,38 +1,143 @@
 import React, { Component } from 'react'
 import '../App.css'
+import Fade from "react-reveal/Fade"
+import ReactModal from "react-modal"
+import Zoom from "react-reveal/Zoom"
+
+
  class Product extends Component {
     
+    constructor(props){
+        super(props)
+        this.state = {
+           product : null
+        }
+    }
+   
+    
+    handleModal = (product) => {
+        this.setState({product})
+    }
+    handleModalClose = () => {
+        this.setState({product:null})
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     render() {
 
+        const {product} = this.state
+
+
         return (
             <div>
+
+            <Fade bottom cascade>
             <ul className="products">
                 {this.props.products.map(product=>(
+
                 <li key={product._id}>
+
                     <div className="product">
-                        <a href="#">
+
+                        <a href="#" onClick={()=>this.handleModal(product)}>
+
                             <img src={product.image}></img>
                             <p>{product.title}</p>
+
                         </a>
+
+
+
+
+
+
+
+
+
+
 
                         <div className="product-price">
                             <div>
                                 ${product.price}
                             </div>
-                            <button 
-                                    onClick={() => this.props.addToCart(product)
-                                    }
-                            className="button primary" 
-                            >Add To Cart</button>
+                            <button  onClick={() => this.props.addToCart(product)} className="button primary" >
+                            Add To Cart</button>
                         </div>
 
                     </div>
                 </li>
                 ))}
             </ul>
-                
+                </Fade>
+
+                    {product && (
+
+
+                    <ReactModal isOpen={this.state.product}
+                        onRequestClose = {this.handleModalClose}
+                    >
+                        <Zoom>
+
+                            <button className="button primary" onClick={this.handleModalClose}>
+                                X
+                                    </button>
+                          <div className="product-details">
+                              <img src={product.image}></img>
+                          </div>
+                          <div className="product-details-description">
+                              <p>
+                                  <strong>{product.title}</strong>
+                              </p>
+                              <p>{product.description}</p>
+                              <p>
+                                  Available Sizes{" "}
+                                  {product.availableSizes.map((x) => 
+                                      (<span>{" "}
+                                      
+                                      <button className="button primary">{x}</button>
+                                                              </span>
+        
+                                      ))}
+                              </p>
+
+                                    <div className="product-price">
+                                        <div>${product.price}</div>
+                                    </div>
+
+                                <button onClick={() => 
+                              {  this.handleModalClose() 
+                                this.props.addToCart(product)}} className="button primary" >
+                                    Add To Cart</button>
+
+                          </div>
+
+                        </Zoom>
+
+
+
+                    </ReactModal>
+
+
+                    )}
+
+
+
+
+
             </div>
         )
     }
