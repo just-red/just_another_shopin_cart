cd2ab4e637aeaf8344c3389fecfebbb624efea22
Raktim Pradip Barua <raktimbarua88@gmail.com>
2020-10-05 15:37:44 +0530

    RaktimPradipBarua

---
+++ src/server.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+const shortid = require("shortid")
+
+
+const app = express()
+app.use(bodyParser)
+
+mongoose.connect("mongodb://localhost/")
